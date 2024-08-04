a4a042026c10715c10ceba7a3acedcea220d88b7
Zhoujianhao <Zhoujianhao>
2024-08-04 22:12:51 +0800

    feat:登录页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Login from '../views/Login.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Login',
+    component: Login
   },
   {
     path: '/about',
