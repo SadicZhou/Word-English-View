3443c443e1d903256c8cc32f7df954dea4465452
zhoujh-F8odz <1372819785@qq.com>
2025-05-06 16:34:36 +0800

    feat:分类管理

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,20 @@ import { initRouter } from './router'
 import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import './styles/theme.scss' // 暂时注释掉，直到创建了这个文件
-import '../src/css/index.scss'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import SzTable from './components/SzTable'
+// import './styles/theme.scss' // 暂时注释掉，直到创建了这个文件
+// import '../src/css/index.scss' // 确认路径和文件是否存在
+
 const pinia = createPinia()
 
 const app = createApp(App)
 initRouter(app)
 app.use(pinia)
-app.use(ElementPlus)
+app.use(ElementPlus, {
+    locale: zhCn
+})
+
+// 注册 SzTable 组件
+app.use(SzTable)
 app.mount('#app')
